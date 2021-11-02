06c9c8b0e5500f1e84e32f52296d4ff7d96ad27b
iberianpig <yhkyky@gmail.com>
2021-11-03 01:22:40 +0900

    fix(appmatcher): use XDG_SESSION_TYPE and XDG_CURRENT_DESKTOP instead of DESKTOP_SESSION

--- lib/fusuma/plugin/appmatcher.rb
+++ lib/fusuma/plugin/appmatcher.rb
@@ -13,11 +13,30 @@ module Fusuma
 
       # @return [Class]
       def backend_klass
-        if ENV['DESKTOP_SESSION'] == 'ubuntu-wayland'
-          Gnome
-        else
-          X11
+        case xdg_session_type
+        when /x11/
+          return X11
+        when /wayland/
+          return Gnome if xdg_current_desktop =~ /GNOME/
         end
+        MultiLogger.error(
+          <<~ERROR
+          appmatcher doesn't support
+          XDG_CURRENT_DESKTOP: '#{xdg_current_desktop}'
+          XDG_SESSION_TYPE: '#{xdg_session_type}'"
+          ERROR
+        )
+        exit 1
+      end
+
+      private
+
+      def xdg_session_type
+        ENV.fetch('XDG_SESSION_TYPE', '')
+      end
+
+      def xdg_current_desktop
+        ENV.fetch('XDG_CURRENT_DESKTOP', '')
       end
     end
   end
--- spec/fusuma/plugin/appmatcher_spec.rb
+++ spec/fusuma/plugin/appmatcher_spec.rb
@@ -2,8 +2,38 @@
 
 require 'spec_helper'
 
-RSpec.describe Fusuma::Plugin::Appmatcher do
-  it 'has a version number' do
-    expect(Fusuma::Plugin::Appmatcher::VERSION).not_to be nil
+module Fusuma
+  module Plugin
+    RSpec.describe Appmatcher do
+      it 'has a version number' do
+        expect(Appmatcher::VERSION).not_to be nil
+      end
+
+      describe '#backend_klass' do
+        context 'when XDG_CURRENT_DESKTOP is UNKNOWN' do
+          before { allow(Appmatcher).to receive(:xdg_current_desktop).and_return('UNKNOWN') }
+          it { expect { Appmatcher.backend_klass }.to raise_error }
+        end
+
+        context 'when XDG_SESSION_TYPE is x11' do
+          before { allow(Appmatcher).to receive(:xdg_session_type).and_return('x11') }
+          it { expect(Appmatcher.backend_klass).to eq Appmatcher::X11 }
+        end
+
+        context 'when XDG_SESSION_TYPE is wayland' do
+          before { allow(Appmatcher).to receive(:xdg_session_type).and_return('wayland') }
+
+          context 'when XDG_CURRENT_DESKTOP is GNOME' do
+            before { allow(Appmatcher).to receive(:xdg_current_desktop).and_return('GNOME') }
+            it { expect(Appmatcher.backend_klass).to eq Appmatcher::Gnome }
+          end
+
+          context 'when XDG_CURRENT_DESKTOP is UNKNOWN' do
+            before { allow(Appmatcher).to receive(:xdg_current_desktop).and_return('UNKNOWN') }
+            it { expect { Appmatcher.backend_klass }.to raise_error }
+          end
+        end
+      end
+    end
   end
 end

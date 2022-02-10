44fab023dd223aa1f55f542831e19ee78a74e8d4
iberianpig <yhkyky@gmail.com>
2022-02-11 02:40:35 +0900

    fix(gnome_extensions): confirm to exist before uninstall

--- lib/fusuma/plugin/appmatcher/gnome_extensions/installer.rb
+++ lib/fusuma/plugin/appmatcher/gnome_extensions/installer.rb
@@ -13,24 +13,41 @@ module Fusuma
 
           def install
             pid = UserSwitcher.new.as_user do |_user|
-              source = File.expand_path(EXTENSION, __dir__)
-              user_extension_dir = File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/")
-
-              FileUtils.cp_r(source, user_extension_dir)
+              FileUtils.cp_r(source_path, user_extension_dir)
               puts "Installed Appmatcher Gnome Shell Extension to #{user_extension_dir}"
               puts "Restart your session, then activate Appmatcher on gnome-extensions-app"
             end
             Process.waitpid(pid)
           end
 
+          def installed?
+            File.exist?(install_path)
+          end
+
           def uninstall
             pid = UserSwitcher.new.as_user do |_user|
-              installed = File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/#{EXTENSION}")
-
-              FileUtils.rm_r(installed)
-              puts "Uninstalled Appmatcher Gnome Shell Extension from #{installed}."
+              if installed?
+                FileUtils.rm_r(install_path)
+                puts "Uninstalled Appmatcher Gnome Shell Extension from #{install_path}."
+              else
+                puts "Appmatcher Gnome Shell Extension is not installed"
+              end
             end
             Process.waitpid(pid)
+          end
+
+          private
+
+          def user_extension_dir
+            File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/")
+          end
+
+          def install_path
+            File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/#{EXTENSION}")
+          end
+
+          def source_path
+            File.expand_path(EXTENSION, __dir__)
           end
         end
       end

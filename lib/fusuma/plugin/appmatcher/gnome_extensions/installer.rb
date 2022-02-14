75ba857b40a7146936f7b111309eb1d8b3d811ff
iberianpig <yhkyky@gmail.com>
2022-02-14 10:57:59 +0900

    fix(gnome_extensions): installer cannot find home dir

--- lib/fusuma/plugin/appmatcher/gnome_extensions/installer.rb
+++ lib/fusuma/plugin/appmatcher/gnome_extensions/installer.rb
@@ -12,23 +12,19 @@ module Fusuma
           EXTENSION = "./appmatcher@iberianpig.dev"
 
           def install
-            pid = UserSwitcher.new.as_user do |_user|
-              FileUtils.cp_r(source_path, user_extension_dir)
-              puts "Installed Appmatcher Gnome Shell Extension to #{user_extension_dir}"
+            pid = UserSwitcher.new.as_user do |user|
+              FileUtils.cp_r(source_path, user_extension_dir(user.username))
+              puts "Installed Appmatcher Gnome Shell Extension to #{user_extension_dir(user.username)}"
               puts "Restart your session, then activate Appmatcher on gnome-extensions-app"
             end
             Process.waitpid(pid)
           end
 
-          def installed?
-            File.exist?(install_path)
-          end
-
           def uninstall
-            pid = UserSwitcher.new.as_user do |_user|
-              if installed?
-                FileUtils.rm_r(install_path)
-                puts "Uninstalled Appmatcher Gnome Shell Extension from #{install_path}."
+            pid = UserSwitcher.new.as_user do |user|
+              if installed?(user.username)
+                FileUtils.rm_r(install_path(user.username))
+                puts "Uninstalled Appmatcher Gnome Shell Extension from #{install_path(user.username)}."
               else
                 puts "Appmatcher Gnome Shell Extension is not installed"
               end
@@ -36,14 +32,18 @@ module Fusuma
             Process.waitpid(pid)
           end
 
-          private
-
-          def user_extension_dir
-            File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/")
+          def installed?(username)
+            File.exist?(install_path(username))
           end
 
-          def install_path
-            File.expand_path("#{Dir.home}/.local/share/gnome-shell/extensions/#{EXTENSION}")
+          private
+
+          def user_extension_dir(username)
+            File.expand_path("#{Dir.home(username)}/.local/share/gnome-shell/extensions/")
+          end
+
+          def install_path(username)
+            File.expand_path("#{Dir.home(username)}/.local/share/gnome-shell/extensions/#{EXTENSION}")
           end
 
           def source_path

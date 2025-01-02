749bc62d6635b1699439f75fc9df2c721b37d6dd
Robert Cambridge <robert@cambridge.me>
2025-01-02 10:11:41 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Fusuma plugin configure app-specific gestures";
+
+  # Add all your dependencies here
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  # Load the blueprint
+  outputs = inputs: inputs.blueprint { inherit inputs; };
+}
